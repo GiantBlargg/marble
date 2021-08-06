39b00e9f4d612fb5f4399d3ac944d66cecb3d67b
Daniel Doran <ddoran3@gmail.com>
2021-08-06 12:53:16 -0600

    cmake tweaks

--- lib/lib.cmake
+++ lib/lib.cmake
@@ -20,7 +20,7 @@ target_link_libraries(libs INTERFACE glfw)
 include(${CMAKE_CURRENT_LIST_DIR}/gl/gl.cmake)
 target_link_libraries(libs INTERFACE gl)
 
-FetchContent_Declare(GLM
+FetchContent_Declare(GLM 
 	URL https://github.com/g-truc/glm/releases/download/0.9.9.8/glm-0.9.9.8.zip
 )
 FetchContent_MakeAvailable(GLM)
@@ -37,14 +37,14 @@ target_link_libraries(imgui gl glfw)
 target_link_libraries(libs INTERFACE imgui)
 
 FetchContent_Declare(json
-	GIT_REPOSITORY https://github.com/nlohmann/json.git
-	GIT_TAG v3.9.1)
-set(JSON_BuildTests OFF)
-set(JSON_Install OFF)
-set(JSON_MultipleHeaders ON)
-set(JSON_ImplicitConversions OFF)
-FetchContent_MakeAvailable(json)
-target_link_libraries(libs INTERFACE nlohmann_json::nlohmann_json)
+	URL https://github.com/nlohmann/json/releases/download/v3.9.1/include.zip
+)
+if(NOT json_POPULATED)
+	FetchContent_Populate(json)
+endif()
+add_library(nlohmann_json INTERFACE)
+target_include_directories(nlohmann_json INTERFACE ${json_SOURCE_DIR}/include)
+target_link_libraries(libs INTERFACE nlohmann_json)
 
 # find_package(OpenAL REQUIRED CONFIG)
 # target_link_libraries(libs INTERFACE OpenAL::OpenAL)
